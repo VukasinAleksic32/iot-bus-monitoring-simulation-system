62597e198c8e33c4711263903b98846e2b999828
Vukasin Aleksic <vukasinaleksic25@gmail.com>
2026-01-25 23:10:32 +0100

    Delete storage.py and remove unnecessary code

--- backend/main.py
+++ backend/main.py
@@ -2,16 +2,6 @@ from flask import Flask
 from flask_cors import CORS
 from server import app as flask_app
 import config
-import os
-
-db_path = "backend/buses.db"
-
-# Reset DB on each simulation start
-if os.path.exists(db_path):
-    os.remove(db_path)
-    print("Database reset: buses.db deleted.")
-else:
-    print("Database file not found, creating a new one.")
 
 CORS(flask_app)
 if __name__ == "__main__":
--- backend/storage.py
+++
@@ -1,7 +0,0 @@
-bus_data = {}
-
-def update(bus_id, data):
-    bus_data[bus_id] = data
-
-def all_buses():
-    return bus_data
